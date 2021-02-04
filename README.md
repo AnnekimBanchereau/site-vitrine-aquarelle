f070c8fdbef7f8544d796b9a03fb4a1ce4bedd88
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-02-04 18:14:49 +0100

    read me updated

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # Site vitrine d'exposition de peintures aquarelle
 
-## techno
+Réalisation d'un site web pour une amie afin de lui permettre de mettre en valeur ses peintures.
+
+![site_web](./site_web.PNG)
+
+## Techno
 
 React
+
+## Fonctionalités
+
+### Première version en cours
+
+* Une page Galerie qui expose l'ensemble des aquarelles
+* Une ou deux page(s) à propos / contact
+* Apparition des peintures au scroll
+* Zoom sur une peinture au click
+* Lien vers Instagram
+* Site responsive
+
+### Deuxième version
+
+* Interface admin pour charger en autonomie les photos
---
+++ src/data/painting.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    title: 'Dicentrarchus Labrax',
+    path: 'dicentrarchus-labrax',
+    alt: 'Accueil',
+  },
+  {
+    title: 'A la fenêtre',
+    path: 'a-la-fenetre',
+    alt: 'Accueil',
+  },
+  {
+    title: 'Ecureuil',
+    path: 'ecureuil',
+    alt: 'sd',
+  },
+  {
+    title: 'Elégante baleine',
+    path: 'elegante-baleine',
+    alt: 'Accueil',
+  },
+  {
+    title: 'Etonnement',
+    path: 'etonnement',
+    alt: 'Accueil',
+  },
+  {
+    title: 'Fleurs colorées',
+    path: 'fleurs-colorees',
+    alt: 'Accueil',
+  },
+  {
+    title: 'Grêve Brehatine',
+    path: 'greve-brehatine',
+    alt: 'Accueil',
+  },
+  {
+    title: 'Hameau breton',
+    path: 'hameau-breton',
+    alt: 'Accueil',
+  },
+  {
+    title: 'Homard',
+    path: 'homard',
+    alt: 'Accueil',
+  },
+  {
+    title: 'A la fenêtre',
+    path: 'a-la-fenetre',
+    alt: 'Accueil',
+  },
+  {
+    title: 'A la fenêtre',
+    path: 'a-la-fenetre',
+    alt: 'Accueil',
+  },
+  {
+    title: 'A la fenêtre',
+    path: 'a-la-fenetre',
+    alt: 'Accueil',
+  },
+  {
+    title: 'A la fenêtre',
+    path: 'a-la-fenetre',
+    alt: 'Accueil',
+  },
+  {
+    title: 'A la fenêtre',
+    path: 'a-la-fenetre',
+    alt: 'Accueil',
+  },
+  {
+    title: 'A la fenêtre',
+    path: 'a-la-fenetre',
+    alt: 'Accueil',
+  },
+];
