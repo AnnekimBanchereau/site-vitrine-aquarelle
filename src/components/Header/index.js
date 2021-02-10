81442d6c68c371cd6c3012dacf32a1603131e251
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-02-10 10:24:39 +0100

    css burger menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles.scss';
 
-const Header = () => (
-  <header className="header">
-    <div className="logo logo_scroll"><a href="">Agathe Chateauminois</a></div>
-    <nav>
-      <ul className="navbar navbar_scroll">
-        <li>
-          <NavLink
-            to="/"
-            activeClassName="navbar_link-active"
-            exact
-          >
-            Galerie
-          </NavLink>
-        </li>
-        <li><NavLink to="/apropos" activeClassName="navbar_link-active">A propos</NavLink></li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toogleBurgerNav = () => {console.log("open")};
+  return (
+    <header className="header">
+      <div className="logo logo_scroll"><a href="">Agathe Chateauminois</a></div>
+      {/* <nav>
+        <ul className="navbar navbar_scroll">
+          <li>
+            <NavLink
+              to="/"
+              activeClassName="navbar_link-active"
+              exact
+            >
+              Galerie
+            </NavLink>
+          </li>
+          <li><NavLink to="/apropos" activeClassName="navbar_link-active">A propos</NavLink></li>
+        </ul>
+      </nav> */}
+      <div className="navburger_block">
+        <span className="navburger_block-title">Menu</span>
+        <ul className="navburger_block-icon" onClick={toogleBurgerNav()}>
+          <span />
+          <span />
+        </ul>
+      </div>
+      {/* <nav className="navburger">
+        <ul className="navburger_link">
+          <li>Galerie</li>
+          <li>A propos</li>
+        </ul>
+      </nav> */}
+    </header>
+  );
+};
 
 export default Header;
